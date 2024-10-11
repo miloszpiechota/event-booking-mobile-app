e8167c3e3f16afb16b1af3e0be577bdefb308b07
MiłoszP <milpie51@gmail.com>
2024-10-11 11:45:15 +0200

    First project configuration - GitHub, extensions, project structure

--- App.js
+++ App.js
@@ -13,7 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
