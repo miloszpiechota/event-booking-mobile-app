ec1e0fdb4be4a05edded41fe45d77d31901ff5bd
MiłoszP <milpie51@gmail.com>
2024-10-11 12:54:30 +0200

    Added Home and Profile navigation using StackNavigation

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from "./navigation/StackNavigator"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <Navigation/>
+    </>
   );
 }
 
@@ -19,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
